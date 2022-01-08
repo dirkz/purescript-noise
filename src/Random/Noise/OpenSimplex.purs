8c62f4e9bcb32f33ec013f0f3068f529463009cf
Dirk Zimmermann <dz@pep.security>
2022-01-08 11:59:34 +0100

    Docs

--- src/Random/Noise/OpenSimplex.purs
+++ src/Random/Noise/OpenSimplex.purs
@@ -1,8 +1,10 @@
+--|Wrapper around [open simplex noise](https://www.npmjs.com/package/open-simplex-noise)
 module Random.Noise.OpenSimplex (makeNoise2D) where
 
 import Data.Function.Uncurried (Fn1, Fn2, runFn1, runFn2)
 
 foreign import _makeNoise2D :: Fn1 Number (Fn2 Number Number Number)
 
+--|Given an arbitrary seed number, returns a 2D noise function.
 makeNoise2D ∷ Number → (Number → Number → Number)
 makeNoise2D seed = runFn2 (runFn1 _makeNoise2D seed)
