9523e3b8f893032ec42d2e90915256685f1b98f1
Dirk Zimmermann <dz@pep.security>
2022-01-08 11:37:07 +0100

    Shorten the test again, significantly

--- test/Noise2D.purs
+++ test/Noise2D.purs
@@ -1,23 +1,17 @@
 module Test.Noise2D (main) where
 
 import Prelude
-
-import Control.Monad.Gen (class MonadGen)
-import Data.ArrayBuffer.Typed.Gen (genFloat64)
 import Effect (Effect)
 import Random.Noise.OpenSimplex (makeNoise2D)
-import Test.QuickCheck (quickCheckGen)
-
-randomNoise2D :: forall m. Bind m => MonadGen m => m (Number -> Number -> Number)
-randomNoise2D = makeNoise2D <$> genFloat64
+import Test.QuickCheck (quickCheck)
 
 testIdemPotence ∷ Effect Unit
 testIdemPotence =
-  quickCheckGen $ do
-    noise2d <- randomNoise2D
-    x <- genFloat64
-    y <- genFloat64
-    pure $ noise2d x y == noise2d x y
+  quickCheck \seed x y ->
+    let
+      noise2d = makeNoise2D seed
+    in
+      noise2d x y == noise2d x y
 
 main ∷ Effect Unit
 main = do
