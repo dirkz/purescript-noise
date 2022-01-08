58abd50a71450b246dc367d17faa47003ee5b591
Dirk Zimmermann <dz@pep.security>
2022-01-08 11:39:26 +0100

    Prove that changing x or y changes the value

--- test/Noise2D.purs
+++ test/Noise2D.purs
@@ -13,6 +13,24 @@ testIdemPotence =
     in
       noise2d x y == noise2d x y
 
+testMovingX ∷ Effect Unit
+testMovingX =
+  quickCheck \seed x1 x2 y ->
+    let
+      noise2d = makeNoise2D seed
+    in
+      noise2d x1 y /= noise2d x2 y
+
+testMovingY ∷ Effect Unit
+testMovingY =
+  quickCheck \seed x y1 y2 ->
+    let
+      noise2d = makeNoise2D seed
+    in
+      noise2d x y1 /= noise2d x y2
+
 main ∷ Effect Unit
 main = do
   testIdemPotence
+  testMovingX
+  testMovingY
