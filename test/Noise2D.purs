afbf72655e1684baf4d1fd002f8b0396f3e4af9c
Dirk Zimmermann <dz@pep.security>
2022-01-08 10:56:28 +0100

    Add tests for moving x, y

--- test/Noise2D.purs
+++ test/Noise2D.purs
@@ -18,6 +18,30 @@ testSameSeed = do
   quickCheck \x y ->
     noise2d x y == noise2d x y
 
+testSameSeedDifferentX ∷ Effect Unit
+testSameSeedDifferentX = do
+  seed <- randomSeed
+  let
+    int = unSeed seed
+
+    num = toNumber int
+  noise2d <- makeNoise2D num
+  quickCheck \x1 x2 y ->
+    noise2d x1 y /= noise2d x2 y
+
+testSameSeedDifferentY ∷ Effect Unit
+testSameSeedDifferentY = do
+  seed <- randomSeed
+  let
+    int = unSeed seed
+
+    num = toNumber int
+  noise2d <- makeNoise2D num
+  quickCheck \x y1 y2 ->
+    noise2d x y1 /= noise2d x y2
+
 main ∷ Effect Unit
 main = do
   testSameSeed
+  testSameSeedDifferentX
+  testSameSeedDifferentY
