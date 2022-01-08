8d2463eae32046e26a71a4e732386dbd4c5b08ea
Dirk Zimmermann <dz@pep.security>
2022-01-08 15:04:10 +0100

    Revert "Try genFloat32"
    This reverts commit 2db8c7bc453ff20d360856728173e6b3fce55123.

--- test.dhall
+++ test.dhall
@@ -2,5 +2,5 @@ let conf = ./spago.dhall
 
 in conf // {
   sources = conf.sources # [ "test/**/*.purs" ],
-  dependencies = conf.dependencies # [ "quickcheck", "arraybuffer", "float32" ]
+  dependencies = conf.dependencies # [ "quickcheck", "arraybuffer" ]
 }
--- test/BigNumber.purs
+++ test/BigNumber.purs
@@ -1,9 +1,7 @@
 module Test.BigNumber where
 
 import Prelude
-
-import Data.ArrayBuffer.Typed.Gen (genFloat32)
-import Data.Float32 (toNumber)
+import Data.ArrayBuffer.Typed.Gen (genFloat64)
 import Test.QuickCheck.Arbitrary (class Arbitrary)
 
 --|Type for floats that cover all the range, not just 0.0-1.0.
@@ -12,4 +10,4 @@ newtype BigNumber
 
 --|Arbitrary instance for our "big-range" float type.
 instance arbitraryBigNumber :: Arbitrary BigNumber where
-  arbitrary = (BigNumber <<< toNumber) <$> genFloat32
+  arbitrary = BigNumber <$> genFloat64
