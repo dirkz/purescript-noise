15223bfc1d5a1590a09843ae9e58d345e344534d
Dirk Zimmermann <dz@pep.security>
2022-01-08 15:21:11 +0100

    Test some parts of 3d noise

--- test/Random/Noise/OpenSimplex.purs
+++ test/Random/Noise/OpenSimplex.purs
@@ -3,7 +3,9 @@ module Test.Random.Noise.OpenSimplex (main) where
 import Prelude
 import Effect (Effect)
 import Test.Random.Noise.OpenSimplex.Noise2D as Noise2D
+import Test.Random.Noise.OpenSimplex.Noise3D as Noise3D
 
 main ∷ Effect Unit
 main = do
   Noise2D.main
+  Noise3D.main
---
+++ test/Random/Noise/OpenSimplex/Noise3D.purs
@@ -0,0 +1,58 @@
+module Test.Random.Noise.OpenSimplex.Noise3D (main) where
+
+import Prelude
+import Effect (Effect)
+import Random.Noise.OpenSimplex (makeNoise3D)
+import Test.QuickCheck (quickCheck, (<?>))
+
+testIdemPotenceInSeed ∷ Effect Unit
+testIdemPotenceInSeed =
+  quickCheck \seed x y z ->
+    let
+      noise1 = makeNoise3D seed
+
+      noise2 = makeNoise3D seed
+    in
+      noise1 x y z == noise2 x y z <?> "testIdemPotenceSeed"
+
+testIdemPotenceInXY ∷ Effect Unit
+testIdemPotenceInXY =
+  quickCheck \seed x y z ->
+    let
+      noise = makeNoise3D seed
+    in
+      noise x y z == noise x y z
+
+testChangingSeed ∷ Effect Unit
+testChangingSeed =
+  quickCheck \seed1 seed2 x y z ->
+    let
+      noise1 = makeNoise3D seed1
+
+      noise2 = makeNoise3D seed2
+    in
+      noise1 x y z /= noise2 x y z <?> "testChangingSeed " <> show seed1 <> " " <> show seed2
+
+testMovingX ∷ Effect Unit
+testMovingX =
+  quickCheck \seed x1 x2 y z ->
+    let
+      noise = makeNoise3D seed
+    in
+      noise x1 y z /= noise x2 y z <?> "testMovingX"
+
+testMovingY ∷ Effect Unit
+testMovingY =
+  quickCheck \seed x y1 y2 z ->
+    let
+      noise = makeNoise3D seed
+    in
+      noise x y1 z /= noise x y2 z <?> "testMovingY"
+
+main ∷ Effect Unit
+main = do
+  testIdemPotenceInSeed
+  testIdemPotenceInXY
+  testChangingSeed
+  testMovingX
+  testMovingY
